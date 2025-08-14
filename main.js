99a51cb77e611a2bdc944226629eb5767e4b6808
nitro-alvin <alvinantonyajish@gmail.com>
2025-08-14 09:07:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function sub() {
+ document.getElementById("submit()").innerHTML = '<h1 class="heading1">Boche യുടെ ജീവിതം ഉമ്പലും കഞ്ഞിയുമായി!</h1> <audio controls autoplay loop><source src="mus.mp3"></audio>'
+ alert("Recharge Completed! Click ok to Complete.")
+}
