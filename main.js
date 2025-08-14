554c1bacd6cb95342f9b0a14752f3e3c8d63e7dd
nitro-alvin <alvinantonyajish@gmail.com>
2025-08-14 09:48:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function sub() {
- document.getElementById("submit()").innerHTML = '<h1 class="heading1">Boche യുടെ ജീവിതം ഉമ്പലും കഞ്ഞിയുമായി!</h1> <audio controls autoplay loop><source src="mus.mp3"></audio>'
+ document.getElementById("submit()").innerHTML = '<h1 class="heading1">നിൻ്റെ ജീവിതം ഉമ്പലും കഞ്ഞിയുമായി!</h1> <audio controls autoplay loop><source src="mus.mp3"></audio>'
  alert("Recharge Completed! Click ok to Complete.")
 }
